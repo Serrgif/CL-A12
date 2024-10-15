90075bb7b4ecc2fa8b1b6e7a527c8f722b3d6a3f
Irina <sergei32git@gmail.com>
2024-10-15 13:10:10 +0500

    Добавление перации проверки соответствия ответа схеме

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,5 +1,5 @@
 package ru.netology.rest;
-
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -16,6 +16,7 @@ class MobileBankApiTestV1 {
                 .get("/demo/accounts")
                 // Проверки
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));;
     }
 }
